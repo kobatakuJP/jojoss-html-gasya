51e684a7ac1d1266afc9dac366d05b2576948fe8
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-04-17 18:30:54 +0900

    gasya and popup

--- src/components/SelectGasyaScene.vue
+++ src/components/SelectGasyaScene.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <button>ガシャを引く</button>
+    <button @click="pullGasha">ガシャを引く</button>
   </div>
 </template>
 
@@ -10,6 +10,9 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class SelectGasyaScene extends Vue {
   @Prop() private msg!: string;
+  pullGasha() {
+    this.$emit("pull", 1)
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
-    <SelectGasyaScene msg="Welcome to Your Vue.js + TypeScript App"/>
+    <SelectGasyaScene @pull="actionPull" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import SelectGasyaScene from '@/components/SelectGasyaScene.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import SelectGasyaScene from "@/components/SelectGasyaScene.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     SelectGasyaScene,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  actionPull(v: number) {
+    alert(v)
+  }
+}
 </script>

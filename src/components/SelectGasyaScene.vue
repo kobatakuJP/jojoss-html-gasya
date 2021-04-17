ab293213e44741d0d307d1d861c978e5d83355da
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-04-17 18:25:18 +0900

    add select-gasya-scene

---
+++ src/components/SelectGasyaScene.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <button>ガシャを引く</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SelectGasyaScene extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+</style>

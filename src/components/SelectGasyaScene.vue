d31b6be67c6fefe146891f2ecbc9098c58d703df
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-04-24 19:14:48 +0900

    #10 display version

--- src/components/SelectGasyaScene.vue
+++ src/components/SelectGasyaScene.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="hello">
+  <div>
+    <div class="version">version:{{ version }}</div>
     <div><button @click="pullGasha(1)">ガシャを引く!💎0個消費</button></div>
-    <div><button @click="pullGasha(10)">10回!!ガシャを引く!💎0個消費</button></div>
+    <div>
+      <button @click="pullGasha(10)">10回!!ガシャを引く!💎0個消費</button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,7 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class SelectGasyaScene extends Vue {
+  version = process.env.VUE_APP_GIT_COMMIT_HASH;
   pullGasha(v: number) {
     this.$emit("pull", v);
   }
@@ -17,4 +21,11 @@ export default class SelectGasyaScene extends Vue {
 </script>
 
 <style scoped>
+.version {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  font-size: 0.8em;
+  font-weight: bold;
+}
 </style>

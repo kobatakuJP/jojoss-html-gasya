fa7f664dafeda42421a68e5d75f55860cdb184b4
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-05-15 18:00:48 +0900

    #49 文字幅攻略

--- src/components/KakuteiMsgComponent.vue
+++ src/components/KakuteiMsgComponent.vue
@@ -23,13 +23,13 @@
         fill="url(#kakutei-grad)"
       />
       <text
-        v-show="isTxt1"
-        ref="txt1"
+        ref="txt"
         :x="w / 2"
         :y="h / 2"
         text-anchor="middle"
         dominant-baseline="central"
         lengthAdjust="spacingAndGlyphs"
+        :textLength="textLength"
         style="
           font-family: Times New Roman;
           font-size: 50;
@@ -38,25 +38,7 @@
           filter: drop-shadow(1px 1px 1px #000);
         "
       >
-        {{ msg }}1
-      </text>
-      <text
-        v-show="!isTxt1"
-        ref="txt2"
-        :x="w / 2"
-        :y="h / 2"
-        text-anchor="middle"
-        dominant-baseline="central"
-        lengthAdjust="spacingAndGlyphs"
-        style="
-          font-family: Times New Roman;
-          font-size: 50;
-          stroke: #86633e;
-          fill: white;
-          filter: drop-shadow(1px 1px 1px #000);
-        "
-      >
-        {{ msg }}2
+        {{ msg }}
       </text>
     </svg>
   </div>
@@ -70,28 +52,20 @@ export default class SingleGasyaButtonComponent extends Vue {
   @Prop() msg!: number;
   readonly w = 792;
   readonly h = 66;
-  isTxt1 = false;
+  textLength: string | null = null;
   mounted() {
     this.resizeFont();
   }
   @Watch("msg")
   changeMsg() {
+    this.textLength = null;
     this.$nextTick(this.resizeFont);
   }
   resizeFont() {
-    const txt1 = this.$refs.txt1 as SVGTextElement;
-    const txt2 = this.$refs.txt2 as SVGTextElement;
-    const [currentText, nextText] = this.isTxt1 ? [txt1, txt2] : [txt2, txt1];
-    // nextを調整して、currentをリセットして、トグルする
-    if (this.w - 10 < currentText.getBBox().width) {
-      // nextText.textLength.baseVal.newValueSpecifiedUnits(
-      //   SVGLength.SVG_LENGTHTYPE_PX,
-      //   this.w - 10
-      // );
-      nextText.setAttribute("textLength", `${this.w - 10}`)
+    const txt = this.$refs.txt as SVGTextElement;
+    if (this.w - 10 < txt.getBBox().width) {
+      this.textLength = `${this.w - 10}`;
     }
-    currentText.removeAttribute("textLength");
-    this.isTxt1 = !this.isTxt1;
   }
 }
 </script>

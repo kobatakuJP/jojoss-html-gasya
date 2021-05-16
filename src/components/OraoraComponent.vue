ec7ece28e11193ee6ee2399f64875328d945a898
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-05-17 00:25:35 +0900

    #15 oraora

---
+++ src/components/OraoraComponent.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="parent100 centering noclick">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="w"
+      :height="h"
+      :viewBox="`0 0 ${w} ${h}`"
+    >
+      <text
+        v-for="i in idxs"
+        :key="i"
+        :ref="`txt`"
+        writing-mode="tb"
+        display="none"
+        @animationend="endOra($event.target)"
+        :rotate="`${i % 2 ? 3 : -3}`"
+        :style="`
+          font-family: Times New Roman;
+          font-weight: bold;
+          font-size: 200;
+          fill: ${i % 2 ? 'black' : 'white'};
+          stroke-width: 2;
+          stroke: ${i % 2 ? 'white' : 'black'};
+        `"
+      >
+        オラ
+      </text>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SingleGasyaButtonComponent extends Vue {
+  readonly w = 792;
+  readonly h = 792 * 1.5;
+  readonly idxs = [...Array(10).keys()];
+  currentIdx = 0;
+  mounted() {
+    this.oraora();
+  }
+  oraora() {
+    setInterval(() => {
+      this.ora(this.currentIdx);
+      this.currentIdx >= this.idxs.length - 1 ? this.currentIdx = 0 : this.currentIdx++;
+    }, 100)
+  }
+  ora(i: number) {
+    const t = (this.$refs[`txt`] as SVGTextElement[])[i];
+    t.setAttribute("x", this.getRand(this.w) + "");
+    t.setAttribute("y", this.getRand(this.h) + "");
+    t.setAttribute("display", "block");
+    t.classList.add("ora-anime");
+  }
+  endOra(t: SVGTextElement) {
+    t.classList.remove("ora-anime");
+    t.setAttribute("display", "none");
+  }
+  getRand(n: number) {
+    return Math.random() * n;
+  }
+}
+</script>
+
+<style scoped>
+.ora-anime {
+  animation: oraora .2s linear;
+}
+@keyframes oraora {
+  0% {
+    opacity: 1;
+  }
+  20% {
+    transform: translateX(1%);
+  }
+  40% {
+    transform: translateX(0%);
+  }
+  60% {
+    transform: translateX(1%);
+  }
+  80% {
+    transform: translateX(0%);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
--- src/components/PunchScene.vue
+++ src/components/PunchScene.vue
@@ -1,12 +1,19 @@
 <template>
-  <div >オラオラオラ</div>
+  <div class="parent100">
+    <oraora-component />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component } from "vue-property-decorator";
-import AbstractScene from "@/components/AbstractScene.vue"
+import AbstractScene from "@/components/AbstractScene.vue";
+import OraoraComponent from "@/components/OraoraComponent.vue";
 
-@Component
+@Component({
+  components: {
+    OraoraComponent,
+  },
+})
 export default class PunchScene extends AbstractScene {
   timeoutForDone = 2000;
 }

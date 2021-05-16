776429c04d1d5874cfefb7002188d0f8ce392482
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-05-16 19:03:26 +0900

    #16 ドギュウゥーン

---
+++ src/components/DogyuunComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="parent100 centering noclick">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="w"
+      :height="h"
+      :viewBox="`0 0 ${w} ${h}`"
+    >
+      <text
+        ref="txt"
+        style="
+          font-family: Times New Roman;
+          font-weight: bold;
+          font-size: 100;
+          fill: black;
+          stroke-width: 4;
+          stroke: white;
+        "
+      >
+        <tspan x="100" y="200" font-size="250" rotate="-45">ド</tspan>
+        <tspan x="200" y="100" font-size="100">ギ</tspan>
+        <tspan x="300" y="100" font-size="80">ュ</tspan>
+        <tspan x="350" y="0" font-size="70">ウ</tspan>
+        <tspan x="450" y="50" font-size="70" rotate="10">ウ</tspan>
+        <tspan x="500" y="100" font-size="100" rotate="30">ー</tspan>
+        <tspan x="600" y="200" font-size="200">ン</tspan>
+      </text>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SingleGasyaButtonComponent extends Vue {
+  readonly w = 792;
+  readonly h = 792;
+}
+</script>
+
+<style scoped>
+.centering {
+  display: flex;
+  justify-content: center; /*左右中央揃え*/
+  align-items: center; /*上下中央揃え*/
+}
+.noclick {
+  pointer-events: none;
+}
+</style>
--- src/components/PuncherAppearScene.vue
+++ src/components/PuncherAppearScene.vue
@@ -1,16 +1,66 @@
 <template>
-  <div >スタープラチナ登場！</div>
+  <div class="parent100 centering">
+    <dogyuun-component class="dogyuun" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component } from "vue-property-decorator";
-import AbstractScene from "@/components/AbstractScene.vue"
+import AbstractScene from "@/components/AbstractScene.vue";
+import DogyuunComponent from "@/components/DogyuunComponent.vue";
 
-@Component
+@Component({
+  components: {
+    DogyuunComponent,
+  },
+})
 export default class PuncherAppearScene extends AbstractScene {
   timeoutForDone = 1000;
 }
 </script>
 
 <style scoped>
+.dogyuun {
+  animation: dogyuun 1s linear;
+}
+.centering {
+  display: flex;
+  justify-content: center; /*左右中央揃え*/
+  align-items: center; /*上下中央揃え*/
+}
+@keyframes dogyuun {
+  0% {
+    width: 0%;
+  }
+  45% {
+    width: 0%;
+  }
+  50% {
+    width: 100%;
+  }
+  55% {
+    transform: translateX(1%);
+  }
+  60% {
+    transform: translateX(0%);
+  }
+  65% {
+    transform: translateX(1%);
+  }
+  70% {
+    transform: translateX(0%);
+  }
+  75% {
+    transform: translateX(1%);
+  }
+  80% {
+    transform: translateX(0%);
+  }
+  85% {
+    transform: translateX(1%);
+  }
+  90% {
+    transform: translateX(0%);
+  }
+}
 </style>

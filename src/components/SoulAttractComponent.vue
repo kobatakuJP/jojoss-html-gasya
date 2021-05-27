c4ddecf2ec28683a053cabd5655d3f53dab8938f
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-05-28 00:41:20 +0900

    魂の覚醒

---
+++ src/components/SoulAttractComponent.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="parent100 centering noclick">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="w"
+      :height="h"
+      :viewBox="`0 0 ${w} ${h}`"
+    >
+      <rect x="0" y="0" :width="w" :height="h" fill="rgba(0,0,0,0.4)" />
+      <circle
+        v-for="(v, i) in xys"
+        :key="`1_${i}`"
+        :ref="`1_${i}`"
+        :cx="v.x"
+        :cy="v.y"
+        stroke="white"
+        fill-opacity="0"
+        stroke-width="2"
+      />
+      <circle
+        v-for="(v, i) in xys"
+        :key="`2_${i}`"
+        :ref="`2_${i}`"
+        :cx="v.x"
+        :cy="v.y"
+        stroke="white"
+        fill-opacity="0"
+        stroke-width="2"
+      />
+      <text
+        :x="w / 2"
+        :y="h / 2"
+        text-anchor="middle"
+        dominant-baseline="central"
+        class="hikareau"
+      >
+        魂
+        <tspan dominant-baseline="mathematical" style="font-size: 70">は</tspan>
+        引かれ合う
+      </text>
+
+      <circle
+        ref="1_last"
+        :cx="w / 2"
+        :cy="h / 2"
+        stroke="white"
+        fill-opacity="0"
+        stroke-width="2"
+      />
+      <circle
+        ref="2_last"
+        :cx="w / 2"
+        :cy="h / 2"
+        stroke="white"
+        fill-opacity="0"
+        stroke-width="2"
+      />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SingleGasyaButtonComponent extends Vue {
+  readonly w = 792;
+  readonly h = 792 * 1.5;
+  xys = [
+    { x: (this.w * 2) / 3, y: this.h / 3 },
+    { x: this.w / 5, y: this.h / 4 },
+    { x: (this.w * 4) / 5, y: (this.h * 2) / 3 },
+    { x: this.w / 4, y: (this.h * 4) / 5 },
+  ];
+  mounted() {
+    const ms = 300;
+    const refs = this.xys.map((_, i) => [
+      this.$refs[`1_${i}`],
+      this.$refs[`2_${i}`],
+    ]);
+    refs.forEach((v, i) => {
+      setTimeout(() => {
+        (v[0] as HTMLElement[])[0].classList.add("hamon");
+      }, ms * i);
+      setTimeout(() => {
+        (v[1] as HTMLElement[])[0].classList.add("hamon");
+      }, ms * i + 100);
+    });
+    debugger
+    const lastRefs = [this.$refs["1_last"], this.$refs["2_last"]];
+    setTimeout(() => {
+      (lastRefs[0] as HTMLElement).classList.add("hamon");
+    }, ms * this.xys.length + 1000);
+    setTimeout(() => {
+      (lastRefs[1] as HTMLElement).classList.add("hamon");
+    }, ms * this.xys.length + 1100);
+  }
+}
+</script>
+
+<style scoped>
+.hikareau {
+  font-family: Times New Roman;
+  text-shadow: -6px 6px 10px white, 1px 1px 5px black;
+  font-weight: bold;
+  font-size: 100px;
+  fill: rgb(135, 206, 235);
+  stroke-width: 2;
+  stroke: white;
+}
+.hamon {
+  animation: hamon 1s ease;
+}
+@keyframes hamon {
+  0% {
+    r: 0;
+  }
+  80% {
+    opacity: 1;
+  }
+  100% {
+    r: 200;
+    opacity: 0;
+  }
+}
+</style>
---
+++ src/components/SoulAttractScene.vue
@@ -0,0 +1,27 @@
+<template>
+  <div @click="onclick">
+    <ResultSceneBackground :rarity="currenRarity"></ResultSceneBackground>
+    <SoulAttractComponent style="position: absolute" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import AbstractScene from "@/components/AbstractScene.vue";
+import ResultSceneBackground from "@/components/ResultSceneBackground.vue";
+import SoulAttractComponent from "@/components/SoulAttractComponent.vue";
+
+@Component({
+  components: {
+    ResultSceneBackground,
+    SoulAttractComponent,
+  },
+})
+export default class LastPunchScene extends AbstractScene {
+  clickToDone = true;
+  //   timeoutForDone = 1000;
+}
+</script>
+
+<style scoped>
+</style>

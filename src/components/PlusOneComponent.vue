4b9a34e50838e7d98e6861225d18023a154b9d27
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2021-06-06 17:56:00 +0900

    #53 add +1

---
+++ src/components/PlusOneComponent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="noclick" :style="styleObj">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="w"
+      :height="h"
+      :viewBox="`0 0 ${w} ${h}`"
+      class="parent100"
+    >
+      <defs>
+        <filter id="plusone-font-shadow">
+          <feDropShadow
+            dx="0"
+            dy="0"
+            :stdDeviation="30"
+            flood-color="yellow"
+          />
+        </filter>
+      </defs>
+      <text
+        :x="w / 2"
+        :y="h * 0.75"
+        text-anchor="middle"
+        dominant-baseline="central"
+        :style="`
+          font-family: Times New Roman;
+          font-weight: bold;
+          font-size: ${w / 4};
+          font-style: oblique;
+            fill: yellow; 
+            stroke-width: 2; 
+            stroke: black
+        `"
+      >
+        ★
+      </text>
+      <text
+        :x="w / 2"
+        :y="h * 0.75"
+        text-anchor="middle"
+        dominant-baseline="central"
+        :style="`
+          font-family: Times New Roman;
+          font-weight: bold;
+          font-size: ${w / 5};
+          font-style: oblique;
+          filter: url(#plusone-font-shadow);
+          fill: red;
+          stroke-width: 1;
+          stroke: gold;
+        `"
+      >
+        +1
+      </text>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class PlusOneComponent extends Vue {
+  @Prop() wp!: number;
+  @Prop() hp!: number;
+  readonly w = this.wp * 10;
+  readonly h = this.hp * 10 * 1.5;
+  get styleObj() {
+    return {
+      position: "absolute",
+      width: `${this.wp}%`,
+      height: `${this.hp}%`,
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/ResultScene.vue
+++ src/components/ResultScene.vue
@@ -1,35 +1,56 @@
 <template>
   <div @click="onclick">
     <ResultSceneBackground :rarity="currenRarity"></ResultSceneBackground>
-    <div v-if="result.length === 1" class="img-wrap" :style="tdStyle(0)" />
+    <div v-if="result.length === 1" class="img-wrap" :style="tdStyle(0)">
+      <PlusOneComponent wp="100" hp="100" v-show="result[0].p1" />
+    </div>
     <div v-else-if="result.length === 10" class="img-wrap">
       <table>
         <tr>
           <td />
-          <td rowspan="2" :style="tdStyle(1)" />
+          <td rowspan="2" :style="tdStyle(1)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[1].p1" />
+          </td>
           <td />
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(2)" />
-          <td rowspan="2" :style="tdStyle(3)" />
+          <td rowspan="2" :style="tdStyle(2)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[2].p1" />
+          </td>
+          <td rowspan="2" :style="tdStyle(3)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[3].p1" />
+          </td>
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(0)" />
+          <td rowspan="2" :style="tdStyle(0)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[0].p1" />
+          </td>
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(4)" />
-          <td rowspan="2" :style="tdStyle(5)" />
+          <td rowspan="2" :style="tdStyle(4)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[4].p1" />
+          </td>
+          <td rowspan="2" :style="tdStyle(5)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[5].p1" />
+          </td>
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(6)" />
+          <td rowspan="2" :style="tdStyle(6)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[6].p1" />
+          </td>
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(7)" />
-
-          <td rowspan="2" :style="tdStyle(8)" />
+          <td rowspan="2" :style="tdStyle(7)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[7].p1" />
+          </td>
+          <td rowspan="2" :style="tdStyle(8)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[8].p1" />
+          </td>
         </tr>
         <tr>
-          <td rowspan="2" :style="tdStyle(9)" />
+          <td rowspan="2" :style="tdStyle(9)">
+            <PlusOneComponent wp="32" hp="25" v-show="result[9].p1" />
+          </td>
         </tr>
         <tr>
           <td />
@@ -44,10 +65,12 @@
 import { Component } from "vue-property-decorator";
 import AbstractScene from "@/components/AbstractScene.vue";
 import ResultSceneBackground from "@/components/ResultSceneBackground.vue";
+import PlusOneComponent from "@/components/PlusOneComponent.vue";
 
 @Component({
   components: {
     ResultSceneBackground,
+    PlusOneComponent,
   },
 })
 export default class ResultScene extends AbstractScene {
@@ -78,5 +101,10 @@ tr {
   height: 12.5%;
   text-align: center;
   vertical-align: middle;
+}
+td {
+  width: 32%;
+  height: 12.5%;
+  vertical-align: top;
 }
 </style>
